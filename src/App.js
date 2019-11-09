8993e63731587e55b03152f226c9f75775831c03
Shorshi <jorgejocampo@gmail.com>
2019-11-09 14:11:03 -0300

    First attemp

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FacebookTopbar from './components/FacebookTopbar';
+
+function App() {
+  return (
+    <div className="App">
+      <FacebookTopbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FacebookTopbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './FacebookTopbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faAngleDown, faCaretDown, faQuestionCircle, faUserFriends, faCommentDots, faBell } from '@fortawesome/free-solid-svg-icons'
+
+export default () => <div className="topbar">
+  <div className="left">
+    <img alt="e" className="logo" src="http://pngimg.com/uploads/facebook_logos/facebook_logos_PNG19753.png"/>
+    <input placeholder="Buscar" className="search-box"></input>
+  </div>
+  <div className="right">
+    <div className="topbar-text right-bordered">
+      Melo
+    </div>
+    <div className="topbar-text right-bordered">
+      Inicio
+    </div>
+    <div className="topbar-text right-bordered">
+      Crear
+    </div>
+    <div className="icon">
+      <FontAwesomeIcon icon={faUserFriends} fixedWidth/>
+    </div>
+    <div className="icon right-bordered">
+    < FontAwesomeIcon icon={faCommentDots} fixedWidth />
+    </div>
+    <div className="icon">
+      <FontAwesomeIcon icon={faBell} fixedWidth  />
+    </div>
+    <div className="icon">
+      <FontAwesomeIcon icon={faQuestionCircle} fixedWidth  />
+    </div>
+    <div className="icon">
+      <FontAwesomeIcon icon={faCaretDown} fixedWidth />
+    </div>
+  </div>
+</div>
